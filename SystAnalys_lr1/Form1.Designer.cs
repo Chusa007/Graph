e96e8a132177f8d181b6856ebb0d8b533c603ad7
Руслан Шамхалов <chusa007@mail.ru>
2016-12-09 23:45:34 +0300

    Доделал рандом

---
+++ SystAnalys_lr1/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace SystAnalys_lr1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxMatrix = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.buttonAdj = new System.Windows.Forms.Button();
+            this.deleteALLButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.drawEdgeButton = new System.Windows.Forms.Button();
+            this.drawVertexButton = new System.Windows.Forms.Button();
+            this.sheet = new System.Windows.Forms.PictureBox();
+            this.BTsave = new System.Windows.Forms.Button();
+            this.BtRandom = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sheet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxMatrix
+            // 
+            this.listBoxMatrix.FormattingEnabled = true;
+            this.listBoxMatrix.Location = new System.Drawing.Point(733, 97);
+            this.listBoxMatrix.Name = "listBoxMatrix";
+            this.listBoxMatrix.Size = new System.Drawing.Size(217, 251);
+            this.listBoxMatrix.TabIndex = 6;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(969, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // selectButton
+            // 
+            this.selectButton.Image = global::SystAnalys_lr1.Properties.Resources.cursor;
+            this.selectButton.Location = new System.Drawing.Point(12, 12);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(45, 45);
+            this.selectButton.TabIndex = 9;
+            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
+            // 
+            // buttonAdj
+            // 
+            this.buttonAdj.Image = global::SystAnalys_lr1.Properties.Resources.smezh;
+            this.buttonAdj.Location = new System.Drawing.Point(733, 39);
+            this.buttonAdj.Name = "buttonAdj";
+            this.buttonAdj.Size = new System.Drawing.Size(92, 52);
+            this.buttonAdj.TabIndex = 7;
+            this.buttonAdj.UseVisualStyleBackColor = true;
+            this.buttonAdj.Click += new System.EventHandler(this.buttonAdj_Click);
+            // 
+            // deleteALLButton
+            // 
+            this.deleteALLButton.Image = global::SystAnalys_lr1.Properties.Resources.deleteAll;
+            this.deleteALLButton.Location = new System.Drawing.Point(13, 217);
+            this.deleteALLButton.Name = "deleteALLButton";
+            this.deleteALLButton.Size = new System.Drawing.Size(45, 45);
+            this.deleteALLButton.TabIndex = 5;
+            this.deleteALLButton.UseVisualStyleBackColor = true;
+            this.deleteALLButton.Click += new System.EventHandler(this.deleteALLButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Image = global::SystAnalys_lr1.Properties.Resources.delete;
+            this.deleteButton.Location = new System.Drawing.Point(13, 166);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(45, 45);
+            this.deleteButton.TabIndex = 3;
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // drawEdgeButton
+            // 
+            this.drawEdgeButton.Image = global::SystAnalys_lr1.Properties.Resources.edge;
+            this.drawEdgeButton.Location = new System.Drawing.Point(12, 115);
+            this.drawEdgeButton.Name = "drawEdgeButton";
+            this.drawEdgeButton.Size = new System.Drawing.Size(45, 45);
+            this.drawEdgeButton.TabIndex = 2;
+            this.drawEdgeButton.UseVisualStyleBackColor = true;
+            this.drawEdgeButton.Click += new System.EventHandler(this.drawEdgeButton_Click);
+            // 
+            // drawVertexButton
+            // 
+            this.drawVertexButton.Image = global::SystAnalys_lr1.Properties.Resources.vertex;
+            this.drawVertexButton.Location = new System.Drawing.Point(13, 64);
+            this.drawVertexButton.Name = "drawVertexButton";
+            this.drawVertexButton.Size = new System.Drawing.Size(45, 45);
+            this.drawVertexButton.TabIndex = 1;
+            this.drawVertexButton.UseVisualStyleBackColor = true;
+            this.drawVertexButton.Click += new System.EventHandler(this.drawVertexButton_Click);
+            // 
+            // sheet
+            // 
+            this.sheet.BackColor = System.Drawing.SystemColors.Control;
+            this.sheet.Location = new System.Drawing.Point(70, 12);
+            this.sheet.Name = "sheet";
+            this.sheet.Size = new System.Drawing.Size(634, 415);
+            this.sheet.TabIndex = 0;
+            this.sheet.TabStop = false;
+            this.sheet.MouseClick += new System.Windows.Forms.MouseEventHandler(this.sheet_MouseClick);
+            // 
+            // BTsave
+            // 
+            this.BTsave.Location = new System.Drawing.Point(848, 39);
+            this.BTsave.Name = "BTsave";
+            this.BTsave.Size = new System.Drawing.Size(93, 48);
+            this.BTsave.TabIndex = 13;
+            this.BTsave.Text = "Сохранить";
+            this.BTsave.UseVisualStyleBackColor = true;
+            this.BTsave.Click += new System.EventHandler(this.BTsave_Click);
+            // 
+            // BtRandom
+            // 
+            this.BtRandom.Location = new System.Drawing.Point(732, 354);
+            this.BtRandom.Name = "BtRandom";
+            this.BtRandom.Size = new System.Drawing.Size(93, 48);
+            this.BtRandom.TabIndex = 14;
+            this.BtRandom.Text = "Рандом";
+            this.BtRandom.UseVisualStyleBackColor = true;
+            this.BtRandom.Click += new System.EventHandler(this.BtRandom_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 444);
+            this.Controls.Add(this.BtRandom);
+            this.Controls.Add(this.BTsave);
+            this.Controls.Add(this.selectButton);
+            this.Controls.Add(this.buttonAdj);
+            this.Controls.Add(this.listBoxMatrix);
+            this.Controls.Add(this.deleteALLButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.drawEdgeButton);
+            this.Controls.Add(this.drawVertexButton);
+            this.Controls.Add(this.sheet);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.sheet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.PictureBox sheet;
+        private System.Windows.Forms.Button drawVertexButton;
+        private System.Windows.Forms.Button drawEdgeButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button deleteALLButton;
+        protected System.Windows.Forms.ListBox listBoxMatrix;
+        private System.Windows.Forms.Button buttonAdj;
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button BTsave;
+        private System.Windows.Forms.Button BtRandom;
+    }
+}
+
